3e07d0edf186b0b6a3303ae14e984c12ba3d08dc
efratzilberlicht <funcakee@gmail.com>
2020-11-23 22:59:35 +0200

    updated homepage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { Switch, Route ,BrowserRouter as Router} from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HopePage';
 import About from './components/About/About';
-import Shiur from './components/Shiur/Shiur';
 import Manager from './components/Manager/Manager';
-import Book from './components/Book/Book';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/components/HomePage/HopePage.jsx
+++ src/components/HomePage/HopePage.jsx
@@ -1,37 +1,45 @@
 import React, { Component } from 'react'
 import connect from 'react-dom'
 import PropTypes from 'prop-types'
-import { Carousel, Form, Col, Button, Card, CardColumns} from 'react-bootstrap'
+import { Carousel, Form, Col, Button, Card, CardColumns } from 'react-bootstrap'
 import Search from '../Search/Search';
 import './HomePage.css';
 
-const IMAGES =[{scr:"/images/HomePage.jpg",position:'First slide' },
-{scr:"/images/shutterstock_1.jpg",position:'Second slide' },
-{scr:"/images/HomePage.jpg",position:'Third slide' },
-{scr:"/images/shutterstock_1.jpg",position:'Fourth slide' }]
+const IMAGES = [{ scr: "/images/HomePage.jpg", position: 'First slide' },
+{ scr: "/images/shutterstock_1.jpg", position: 'Second slide' },
+{ scr: "/images/HomePage.jpg", position: 'Third slide' },
+{ scr: "/images/shutterstock_1.jpg", position: 'Fourth slide' }]
 
-const ICONS = [{src:"./images/poriut.png" , name:"פוריות ויוחסין"},
-{src:"./images/actualation.png" , name:"אקטואליה בהלכה"},
-{src:"./images/yearcircle.png"  , name:"מעגל השנה"},
-{src:"./images/weaman.png" , name:"חיובי נשים"},
-{src:"./images/life.png" , name:"אורח חיים"},
-{src:"./images/shabat.png" , name:"שבת"},
-{src:"./images/chehuna.png" , name:"כהונה"},
-{src:"./images/avelut.png" , name:"אבלות"},
-{src:"./images/machshava.png" , name:"מחשבה"}]
+const ICONS = [{ src: "./images/poriut.png", name: "פוריות ויוחסין" },
+{ src: "./images/actualation.png", name: "אקטואליה בהלכה" },
+{ src: "./images/yearcircle.png", name: "מעגל השנה" },
+{ src: "./images/weaman.png", name: "חיובי נשים" },
+{ src: "./images/life.png", name: "אורח חיים" },
+{ src: "./images/shabat.png", name: "שבת" },
+{ src: "./images/chehuna.png", name: "כהונה" },
+{ src: "./images/avelut.png", name: "אבלות" },
+{ src: "./images/machshava.png", name: "מחשבה" }]
 
-const UPDATES = [{src:"./images/update.png",title:"חדשות", 
-text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
-, time:"שעות 3" },
-{src:"./images/update.png",title:"השיעור החדש", 
-text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
-, time:"שעות 7 " },
-{src:"./images/update.png",title:"המאמר החודשי", 
-text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
-, time:"שעות 10" },
-{src:"./images/update.png",title:"שאלות ותשובות", 
-text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
-, time:"שעות 5" }]
+const UPDATES = [{
+    src: "./images/update.png", title: "חדשות",
+    text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
+    , time: "שעות 3"
+},
+{
+    src: "./images/update.png", title: "השיעור החדש",
+    text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
+    , time: "שעות 7 "
+},
+{
+    src: "./images/update.png", title: "המאמר החודשי",
+    text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
+    , time: "שעות 10"
+},
+{
+    src: "./images/update.png", title: "שאלות ותשובות",
+    text: "This is a wider card with supporting text below as a natural additional content. with supporting text below as"
+    , time: "שעות 5"
+}]
 
 export default function HomePage(props) {
 
@@ -48,56 +56,73 @@ export default function HomePage(props) {
                 </Carousel.Caption>
             </Carousel.Item>
         );
-      }
+    }
 
     function getIconCard(src, name) {
-        return(
-         <Card>
-        <Card.Body>
-            <Card.Img src={src} className="s"/>
-            <Card.Title className="t">{name}</Card.Title>
-        </Card.Body>
-    </Card>
-      );
-    }
-    
-    function getUpdate(src, title, text, time){
-        return(<Card>
-           <Card.Body className="updateCard">
-           <Card.Img src={src} className="updateImg" />
-             <Card.Title>{title}</Card.Title>
-             <Card.Text>
-             {text}
-            <br />
-        <small className="text-muted"> עודכן לפני  {time} </small>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Img src={src} className="s" />
+                    <Card.Title className="t">{name}</Card.Title>
+                </Card.Body>
+            </Card>
         );
     }
 
-    return (<div>
-        <Carousel>
-        {IMAGES.map(image => getCarouselItem(image.src, image.position))}
-        </Carousel>
+    // function getUpdate(src, title, text, time){
+    //     return(<Card>
+    //        <Card.Body className="updateCard">
+    //        <Card.Img src={src} className="updateImg" />
+    //          <Card.Title>{title}</Card.Title>
+    //          <Card.Text>
+    //          {text}
+    //         <br />
+    //     <small className="text-muted"> עודכן לפני  {time} </small>
+    //         </Card.Text>
+    //       </Card.Body>
+    //     </Card>
+    //     );
+    // }
+    function getUpdate(src, title, text, time) {
+        return (
+            <div className="col-lg-2">
+               
+                    <Card.Body className="updateCard">
+                        <Card.Img src={src} className="updateImg" />
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                            {text}
+                            <br />
+                            <small className="text-muted"> עודכן לפני  {time} </small>
+                        </Card.Text>
+                    </Card.Body>
+               </div>
+        );
+    }
+
+    return (<div id="row">
         
-        <div id="search">
+        <Carousel>
+            {IMAGES.map(image => getCarouselItem(image.src, image.position))}
+        </Carousel>
+
+      <div id="search">
             <h1 className="h1">חפש נושא</h1>
             <h5> תוכל למצוא בכל השיעורים בהלכה ובמחשבה</h5>
-           <Search/>
-        </div>
+            <Search />
+        </div> 
 
         <div id="subjects">
             <h1> ? איזה נושא מעניין אותך </h1>
-           <div id="icons">  
-           <CardColumns>
+            <div id="icons">
+                <CardColumns>
                     {ICONS.map(icon => getIconCard(icon.src, icon.name))}
-            </CardColumns>
-           </div>
+                </CardColumns>
+            </div>
         </div>
         <div id="news">
-            <h1 className="h1">!רוצה להיות מעודכן בתוכן החדש שלנו? הרשם עכשיו</h1>
-            <Form dir="rtl">
+            <h1 className="h1 col-md-12">!רוצה להיות מעודכן בתוכן החדש שלנו? הרשם עכשיו</h1>
+           <Form dir="rtl">
                 <Form.Row>
                     <Col>
                         <Form.Control className="input" placeholder="שם" />
@@ -115,9 +140,13 @@ export default function HomePage(props) {
         <div dir="rtl" id="updates">
             <h1 className="title">עדכונים</h1>
             <img alt="kavyaeli" src="/images/kavyaeli.png" className="kav" />
-            <CardColumns id="cardColumns">
+            {/* <CardColumns id="cardColumns">
                 {UPDATES.map(update => getUpdate(update.src, update.title, update.text, update.time))}
-            </CardColumns>
+            </CardColumns> */}
+            <div className="row">
+            <div className="col-sm-2"></div>
+                {UPDATES.map(update => getUpdate(update.src, update.title, update.text, update.time))}
+       </div>
         </div>
 
     </div>);
