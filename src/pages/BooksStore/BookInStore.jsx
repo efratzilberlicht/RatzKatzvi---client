b594a0fd04fa4499dd2ae94a4c37bdd2e5533df3
efratzilberlicht <funcakee@gmail.com>
2020-12-06 15:26:20 +0200

    background image

--- src/pages/BooksStore/BookInStore.jsx
+++ src/pages/BooksStore/BookInStore.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-// import booksback from '../../images/booksback.png';
 import './BooksStore.css';
 
 
 export default function BookInStore(props) {
 
     return (
-           <div className="bookInStore align-items-md-center"
-            // style={{backgroundImage:`url(${booksback})`}}
-            >
+           <div className="bookInStore align-items-md-center">
+            
                 <img src={props.book.src} className="bookImg" alt="book"/>
                 <p>{props.book.name}</p>
                 
                <p id="price"> {props.book.price} ש"ח
                 </p>
-
+               {/* <button type="submit" className="addTOoCart">הוספה לסל</button> */}
                 <Button variant="success" className="addTOoCart">הוספה לסל</Button>
            </div>
     );
