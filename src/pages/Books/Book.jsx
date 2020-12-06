bf458d2926ff7e6796d71fb1240572ee65814f8e
bl <milgaplus@gmail.com>
2020-12-06 13:50:14 +0200

    download books

--- src/pages/Books/Book.jsx
+++ src/pages/Books/Book.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { Card, Button } from 'react-bootstrap'
-import './Books.css';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import FileSaver from "file-saver";
+import "./Books.css";
 
 export default function Book(props) {
-
-    return (
-           <div className="book align-items-md-center">
-                <img src={props.book.src} className="bookImg" alt="book"/>
-                <p>{props.book.name}</p>
-                <Button variant="success" className="download"> להורדת PDF</Button>
-           </div>
-    );
+  const saveFile = () => {
+    FileSaver.saveAs(props.srcPDF, props.srcPDF);
+  };
+  const newWindow = () => {
+    let newWindow = window.open(props.srcPDF);
+  };
+  return (
+    <div className="book align-items-md-center">
+      <img
+        src={props.book.src}
+        className="bookImg"
+        alt="book"
+        onClick={newWindow}
+      />
+      <p>{props.book.name}</p>
+      <Button variant="success" className="download" onClick={saveFile}>
+        {" "}
+        להורדת PDF
+      </Button>
+    </div>
+  );
 }
